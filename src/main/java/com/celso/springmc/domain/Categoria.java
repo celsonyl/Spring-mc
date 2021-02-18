bca950dc388c7f40ff4e2785822a0a4a97f29d18
Celso Bertolotto <celsonyl@hotmail.com>
2021-02-18 13:14:28 -0300

    Testando rest

---
+++ src/main/java/com/celso/springmc/domain/Categoria.java
@@ -0,0 +1,46 @@
+package com.celso.springmc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+
+    private int id;
+    private String nome;
+
+    public Categoria(){}
+
+    public Categoria(int id,String nome){
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id == categoria.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
