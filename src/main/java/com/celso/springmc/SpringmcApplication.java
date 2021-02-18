fdd853d5cd616bc6c70001d93759ede78bcb040e
Celso Bertolotto <celsonyl@hotmail.com>
2021-02-18 13:06:28 -0300

    Projeto criado

---
+++ src/main/java/com/celso/springmc/SpringmcApplication.java
@@ -0,0 +1,13 @@
+package com.celso.springmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmcApplication.class, args);
+	}
+
+}
