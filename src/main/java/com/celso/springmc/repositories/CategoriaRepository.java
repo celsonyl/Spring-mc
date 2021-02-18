0c3f9b034a95508add3853189af10268b7bbd72f
Celso Bertolotto <celsonyl@hotmail.com>
2021-02-18 13:58:04 -0300

    banco h2

---
+++ src/main/java/com/celso/springmc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.celso.springmc.repositories;
+
+import com.celso.springmc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Id;
+
+@Repository
+public class CategoriaRepository {
+
+}
--- src/main/java/com/celso/springmc/resources/CategoriaResource.java
+++ src/main/java/com/celso/springmc/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package com.celso.springmc.resources;
 
+import com.celso.springmc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "Funciono";
+    public List<Categoria> listar(){
+        List<Categoria> list = new ArrayList<>();
+        Categoria c1 = new Categoria(1,"Celso");
+        Categoria c2 = new Categoria(2,"Robertin");
+        list.add(c1);
+        list.add(c2);
+
+
+        return list;
     }
 
 }
