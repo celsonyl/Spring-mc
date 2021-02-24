cb2d48bdb995c337af63c190801967c9bbcc2503
Celso Bertolotto <celsonyl@hotmail.com>
2021-02-24 10:51:53 -0300

    personalizando erro de validacao

--- src/main/java/com/celso/springmc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/celso/springmc/resources/exception/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import com.celso.springmc.services.exceptions.DataIntegrityException;
 import com.celso.springmc.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -23,6 +25,17 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> dataViolation(DataIntegrityException error,HttpServletRequest request){
         StandardError erro = new StandardError(HttpStatus.BAD_REQUEST.value(), error.getMessage() , System.currentTimeMillis());
 
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> methodValidation(MethodArgumentNotValidException error,HttpServletRequest request){
+        ValidationError erro = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação" , System.currentTimeMillis());
+
+        for(FieldError x : error.getBindingResult().getFieldErrors() ){
+            erro.addError(x.getField(),x.getDefaultMessage());
+        }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
     }
 
---
+++ src/main/java/com/celso/springmc/resources/exception/ValidationError.java
@@ -0,0 +1,22 @@
+package com.celso.springmc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError{
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError(Integer status, String msg, Long timeStamp) {
+        super(status, msg, timeStamp);
+
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldname,String message) {
+        errors.add(new FieldMessage(fieldname,message));
+    }
+}
